5ca9a747f52a6af370e8cb4c6368ec4d5e18907e
Mihai Cozmuta <mihai.cozmuta@tecknoworks.com>
2020-03-05 10:47:47 +0200

    added more methods to my tree

--- trees.py
+++ trees.py
@@ -23,7 +23,29 @@ def pre_order(node):
     pre_order(node.right)
 
 
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.item, " ")
+    in_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.item, " ")
+
+
 if __name__ == '__main__':
     my_array = [1, 2, 3, 4, 5, 6, 7]
     my_first_tree = bst_from_sorted_array(my_array)
+    print('Here comes the pre-order traversal')
     pre_order(my_first_tree)
+    print('Here comes the in-order traversal')
+    in_order(my_first_tree)
+    print('Here comes the post-order traversal')
+    post_order(my_first_tree)
+    print("The root is:", my_first_tree.item)
